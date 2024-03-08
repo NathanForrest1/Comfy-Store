94d41dd47345d25e849b9b8b72ae03038e08b47c
Nathan Forrest <0123nate23@gmail.com>
2024-03-08 11:28:08 -0600

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,33 @@
+const About = () => {
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We Love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              Comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        At Comfy Store, we're dedicated to redefining the meaning of comfort in
+        your home. Our journey began with a simple mission: to curate a
+        collection of furniture, house decorations, and beds that not only exude
+        style but also prioritize unparalleled comfort. With a passion for
+        creating inviting living spaces, we meticulously select each piece to
+        ensure it meets our high standards of quality and coziness. Whether
+        you're seeking the perfect sofa to sink into after a long day or looking
+        to add a touch of warmth with our decor accents, Comfy Store is here to
+        help you transform your space into a sanctuary of relaxation. Our
+        commitment to exceptional customer service means that your comfort and
+        satisfaction are our top priorities. Join us in embracing the art of
+        comfort at Comfy Store.
+      </p>
+    </>
+  );
+};
+export default About;
